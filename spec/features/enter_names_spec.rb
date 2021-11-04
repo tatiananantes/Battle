903b2349a6a3753f2017dcea7a986ab34fe96206
Tatiana Nantes <tatiananantes@gmail.com>
2021-11-04 17:52:15 +0000

    extract repeating code to test helper file

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,19 +1,12 @@
 
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Charlotte'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
-    # save_and_open_page
+    sign_in_and_play
     expect(page).to have_content 'Charlotte vs. Mittens'
   end
 
   scenario 'page redirect' do
-    visit('/')
-    fill_in :player_1_name, with: 'Charlotte'
-    fill_in :player_2_name, with: 'Mittens'
-    click_button 'Submit'
+    sign_in_and_play
     expect(current_path).to eq '/play'
   end
 end 

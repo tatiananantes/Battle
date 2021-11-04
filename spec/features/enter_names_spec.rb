d89baea4bc7953089de7bf684f1a997cec5c176c
Tatiana Nantes <tatiananantes@gmail.com>
2021-11-04 16:57:48 +0000

    implement POST/redirect/GET pattern

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -5,9 +5,18 @@ feature 'Enter names' do
     fill_in :player_1_name, with: 'Charlotte'
     fill_in :player_2_name, with: 'Mittens'
     click_button 'Submit'
-
-    save_and_open_page
-    
+    # save_and_open_page
     expect(page).to have_content 'Charlotte vs. Mittens'
   end
-end
+
+  scenario 'page redirect' do
+    visit('/')
+    fill_in :player_1_name, with: 'Charlotte'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+    expect(current_path).to eq '/play'
+  end
+
+
+
+end 

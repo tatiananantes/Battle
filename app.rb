89ed1591ec185a91687d3f4749c0f7f27fa0a89e
Tatiana Nantes <tatiananantes@gmail.com>
2021-11-04 12:22:58 +0000

    fix issue with failing test

--- app.rb
+++ app.rb
@@ -11,8 +11,8 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    @player_1_name = params [:player_1_name]
-    @player_2_name = params [:player_2_name]
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
     erb :play
   end
 

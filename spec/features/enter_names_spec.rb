5cc3a6ad46414ffdc91a728e3a464337ca022ec9
Tatiana Nantes <tatiananantes@gmail.com>
2021-11-04 14:35:08 +0000

    add entering players code II - complete user story

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -5,6 +5,9 @@ feature 'Enter names' do
     fill_in :player_1_name, with: 'Charlotte'
     fill_in :player_2_name, with: 'Mittens'
     click_button 'Submit'
+
+    save_and_open_page
+    
     expect(page).to have_content 'Charlotte vs. Mittens'
   end
 end
